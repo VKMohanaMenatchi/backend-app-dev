f3284afc2127878206f124a5b8ec22ac75208469
VKMohanaMenatchi <mohanamenatchivk03@gmail.com>
2025-10-28 12:12:14 +0530

    Update WebSecurityConfig.java

--- src/main/java/com/examly/springapp/config/WebSecurityConfig.java
+++ src/main/java/com/examly/springapp/config/WebSecurityConfig.java
@@ -75,7 +75,7 @@ public class WebSecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://frontend-app-dev-six.vercel.app/"));
+        configuration.setAllowedOrigins(Arrays.asList("https://frontend-app-dev-six.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
